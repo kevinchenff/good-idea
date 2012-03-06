8f0c130a518f4ade20c1071ae0cde9f2946c251c
zhangwy0916@gmail.com <zhangwy0916@gmail.com@e6631ee2-bc36-6fc4-7263-08c298121e4e>
2012-03-06 16:26:45 +0000

    增加知识选用模式对话框，需要加紧熟悉MFC的开发方法

---
+++ ChooseFSTTypeDLL/ChooseFSTType/ChooseFSTType/FstKnowledgeBasedDlg.cpp
@@ -0,0 +1,102 @@
+// FstKnowledgeBasedDlg.cpp : implementation file
+//
+
+#include "stdafx.h"
+#include "ChooseFSTType.h"
+#include "FstKnowledgeBasedDlg.h"
+
+
+// FstKnowledgeBasedDlg dialog
+
+IMPLEMENT_DYNAMIC(FstKnowledgeBasedDlg, CDialog)
+
+FstKnowledgeBasedDlg::FstKnowledgeBasedDlg(CWnd* pParent /*=NULL*/)
+	: CDialog(FstKnowledgeBasedDlg::IDD, pParent)
+{
+
+}
+
+FstKnowledgeBasedDlg::~FstKnowledgeBasedDlg()
+{
+}
+
+void FstKnowledgeBasedDlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialog::DoDataExchange(pDX);
+}
+
+
+BEGIN_MESSAGE_MAP(FstKnowledgeBasedDlg, CDialog)
+	ON_WM_CTLCOLOR()
+	ON_WM_PAINT()
+END_MESSAGE_MAP()
+
+
+// FstKnowledgeBasedDlg message handlers
+HBRUSH FstKnowledgeBasedDlg::OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor)
+{
+	HBRUSH hbr = CDialog::OnCtlColor(pDC, pWnd, nCtlColor);
+	if(nCtlColor==CTLCOLOR_BTN)  
+	{
+		pDC->SetTextColor(RGB(0,0,0));
+		pDC->SetBkColor(RGB(250,230,200));
+		HBRUSH b=CreateSolidBrush(RGB(250,230,200));
+		return b;
+	}
+	else if(nCtlColor==CTLCOLOR_SCROLLBAR)
+	{
+		pDC->SetTextColor(RGB(0,0,0));
+		pDC->SetBkColor(RGB(250,230,200));
+		HBRUSH b=CreateSolidBrush(RGB(250,230,200));
+		return b;
+	}
+	else if(nCtlColor==CTLCOLOR_EDIT)
+	{;
+	pDC->SetTextColor(RGB(0,0,0));
+	pDC->SetBkColor(RGB(255,255,255));
+	HBRUSH b=CreateSolidBrush(RGB(255,255,255));
+	return b;
+	}
+	else if(nCtlColor==CTLCOLOR_STATIC)
+	{
+		pDC->SetTextColor(RGB(0,0,0));
+		pDC->SetBkColor(RGB(250,230,200));
+		HBRUSH b=CreateSolidBrush(RGB(250,230,200));
+		return b;
+	}
+	else if(nCtlColor==CTLCOLOR_DLG)
+	{
+		pDC->SetTextColor(RGB(0,0,0));
+		pDC->SetBkColor(RGB(250,230,200));
+		HBRUSH b=CreateSolidBrush(RGB(250,230,200));
+		return b;
+	}
+
+	else if(nCtlColor==CTLCOLOR_MSGBOX )
+	{
+		pDC->SetTextColor(RGB(0,0,0));
+		pDC->SetBkColor(RGB(250,230,200));
+		HBRUSH b=CreateSolidBrush(RGB(250,230,200));
+		return b;
+	}
+	return hbr;
+}
+
+void FstKnowledgeBasedDlg::OnPaint()
+{
+	CPaintDC dc(this); // device context for painting
+	CDC dcMem; 
+	dcMem.CreateCompatibleDC(&dc); 
+
+	CRect rect1;
+	GetClientRect(&rect1); 		
+
+	CBitmap m_bitmap;
+	m_bitmap.LoadBitmap(IDB_BITBackground);
+
+	BITMAP bitMap;
+	m_bitmap.GetBitmap(&bitMap);
+
+	CBitmap *pbmpOld=dcMem.SelectObject(&m_bitmap);  
+	dc.StretchBlt(0,(rect1.Height()-bitMap.bmHeight),bitMap.bmWidth,bitMap.bmHeight,&dcMem,0,0,bitMap.bmWidth,bitMap.bmHeight,SRCCOPY); 
+}
