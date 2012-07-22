e9daf168f582ff278ed05629ca3360c6f21a7c03
zhangwy0916@gmail.com <zhangwy0916@gmail.com@e6631ee2-bc36-6fc4-7263-08c298121e4e>
2012-07-22 13:16:51 +0000

    完成知识选择的入口界面

--- MBDPrtFW/MBDPrtFstDesignMod.m/src/PrtFstFreeStyleDlg.cpp
+++ MBDPrtFW/MBDPrtFstDesignMod.m/src/PrtFstFreeStyleDlg.cpp
@@ -31,12 +31,28 @@
 PrtFstFreeStyleDlg::PrtFstFreeStyleDlg() :
   CATDlgDialog ((CATApplicationFrame::GetApplicationFrame())->GetMainWindow(),
 //CAA2 WIZARD CONSTRUCTOR DECLARATION SECTION
-"PrtFstFreeStyleDlg",CATDlgWndBtnOKCancel|CATDlgGridLayout
+"PrtFstFreeStyleDlg",CATDlgWndBtnClose|CATDlgGridLayout
 //END CAA2 WIZARD CONSTRUCTOR DECLARATION SECTION
                                )
 {
 //CAA2 WIZARD CONSTRUCTOR INITIALIZATION SECTION
  _Frame001 = NULL;
+ _Label01 = NULL;
+ _Label02 = NULL;
+ _Label03 = NULL;
+ _Label04 = NULL;
+ _Label05 = NULL;
+ _Combo01 = NULL;
+ _Combo02 = NULL;
+ _Combo03 = NULL;
+ _Editor01 = NULL;
+ _Editor02 = NULL;
+ _GoToSearchPB = NULL;
+ _Frame002 = NULL;
+ _SearchResultsML = NULL;
+ _Frame004 = NULL;
+ _NextStepPB = NULL;
+ _LastStepPB = NULL;
 //END CAA2 WIZARD CONSTRUCTOR INITIALIZATION SECTION
 
 
@@ -53,6 +69,22 @@ PrtFstFreeStyleDlg::~PrtFstFreeStyleDlg()
 //  --------------------------------------------------
 //CAA2 WIZARD DESTRUCTOR DECLARATION SECTION
  _Frame001 = NULL;
+ _Label01 = NULL;
+ _Label02 = NULL;
+ _Label03 = NULL;
+ _Label04 = NULL;
+ _Label05 = NULL;
+ _Combo01 = NULL;
+ _Combo02 = NULL;
+ _Combo03 = NULL;
+ _Editor01 = NULL;
+ _Editor02 = NULL;
+ _GoToSearchPB = NULL;
+ _Frame002 = NULL;
+ _SearchResultsML = NULL;
+ _Frame004 = NULL;
+ _NextStepPB = NULL;
+ _LastStepPB = NULL;
 //END CAA2 WIZARD DESTRUCTOR DECLARATION SECTION
 
 
@@ -67,10 +99,55 @@ void PrtFstFreeStyleDlg::Build()
 //  -------------------------------------------------------------------
 
 //CAA2 WIZARD WIDGET CONSTRUCTION SECTION
- SetGridRowResizable(0,1);
+ SetGridRowResizable(1,1);
  SetGridColumnResizable(0,1);
- _Frame001 = new CATDlgFrame(this, "Frame001", CATDlgFraNoTitle|CATDlgGridLayout);
+ _Frame001 = new CATDlgFrame(this, "Frame001", CATDlgGridLayout);
 _Frame001 -> SetGridConstraints(0, 0, 1, 1, CATGRID_4SIDES);
+ _Frame001 -> SetGridColumnResizable(1,1);
+ _Label01 = new CATDlgLabel(_Frame001, "Label01");
+_Label01 -> SetGridConstraints(0, 0, 1, 1, CATGRID_4SIDES);
+ _Label02 = new CATDlgLabel(_Frame001, "Label02");
+_Label02 -> SetGridConstraints(1, 0, 1, 1, CATGRID_4SIDES);
+ _Label03 = new CATDlgLabel(_Frame001, "Label03");
+_Label03 -> SetGridConstraints(2, 0, 1, 1, CATGRID_4SIDES);
+ _Label04 = new CATDlgLabel(_Frame001, "Label04");
+_Label04 -> SetGridConstraints(3, 0, 1, 1, CATGRID_4SIDES);
+ _Label05 = new CATDlgLabel(_Frame001, "Label05");
+_Label05 -> SetGridConstraints(4, 0, 1, 1, CATGRID_4SIDES);
+ _Combo01 = new CATDlgCombo(_Frame001, "Combo01", CATDlgCmbDropDown);
+_Combo01 -> SetGridConstraints(0, 1, 1, 1, CATGRID_4SIDES);
+ _Combo02 = new CATDlgCombo(_Frame001, "Combo02", CATDlgCmbDropDown);
+_Combo02 -> SetGridConstraints(1, 1, 1, 1, CATGRID_4SIDES);
+ _Combo03 = new CATDlgCombo(_Frame001, "Combo03", CATDlgCmbDropDown);
+_Combo03 -> SetGridConstraints(2, 1, 1, 1, CATGRID_4SIDES);
+ _Editor01 = new CATDlgEditor(_Frame001, "Editor01");
+_Editor01 -> SetGridConstraints(3, 1, 1, 1, CATGRID_4SIDES);
+ _Editor02 = new CATDlgEditor(_Frame001, "Editor02");
+_Editor02 -> SetGridConstraints(4, 1, 1, 1, CATGRID_4SIDES);
+ _GoToSearchPB = new CATDlgPushButton(_Frame001, "GoToSearchPB");
+_GoToSearchPB -> SetGridConstraints(5, 1, 1, 1, CATGRID_4SIDES);
+ _Frame002 = new CATDlgFrame(this, "Frame002", CATDlgGridLayout);
+_Frame002 -> SetGridConstraints(1, 0, 1, 1, CATGRID_4SIDES);
+ _Frame002 -> SetGridRowResizable(0,1);
+ _Frame002 -> SetGridColumnResizable(0,1);
+ _SearchResultsML = new CATDlgMultiList(_Frame002, "SearchResultsML");
+ CATUnicodeString SearchResultsMLTitles [ 6 ];
+ SearchResultsMLTitles[0] = CATMsgCatalog::BuildMessage("PrtFstFreeStyleDlg", "Frame002.SearchResultsML.ColumnTitle1");
+ SearchResultsMLTitles[1] = CATMsgCatalog::BuildMessage("PrtFstFreeStyleDlg", "Frame002.SearchResultsML.ColumnTitle2");
+ SearchResultsMLTitles[2] = CATMsgCatalog::BuildMessage("PrtFstFreeStyleDlg", "Frame002.SearchResultsML.ColumnTitle3");
+ SearchResultsMLTitles[3] = CATMsgCatalog::BuildMessage("PrtFstFreeStyleDlg", "Frame002.SearchResultsML.ColumnTitle4");
+ SearchResultsMLTitles[4] = CATMsgCatalog::BuildMessage("PrtFstFreeStyleDlg", "Frame002.SearchResultsML.ColumnTitle5");
+ SearchResultsMLTitles[5] = CATMsgCatalog::BuildMessage("PrtFstFreeStyleDlg", "Frame002.SearchResultsML.ColumnTitle6");
+ _SearchResultsML -> SetColumnTitles(6, SearchResultsMLTitles);
+ _SearchResultsML -> SetVisibleColumnCount( 6 );
+_SearchResultsML -> SetGridConstraints(0, 0, 1, 1, CATGRID_4SIDES);
+ _Frame004 = new CATDlgFrame(this, "Frame004", CATDlgFraNoFrame|CATDlgGridLayout);
+_Frame004 -> SetGridConstraints(2, 0, 1, 1, CATGRID_4SIDES);
+ _Frame004 -> SetGridColumnResizable(0,1);
+ _NextStepPB = new CATDlgPushButton(_Frame004, "NextStepPB");
+_NextStepPB -> SetGridConstraints(0, 1, 1, 1, CATGRID_4SIDES);
+ _LastStepPB = new CATDlgPushButton(_Frame004, "LastStepPB");
+_LastStepPB -> SetGridConstraints(0, 0, 1, 1, CATGRID_RIGHT|CATGRID_TOP|CATGRID_BOTTOM);
 //END CAA2 WIZARD WIDGET CONSTRUCTION SECTION
 
 
