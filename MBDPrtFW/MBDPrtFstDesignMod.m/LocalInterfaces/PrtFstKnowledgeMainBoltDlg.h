2f62ebba413eb92d9f27e0ebaad3ede195dd8705
zhangwy0916@gmail.com <zhangwy0916@gmail.com@e6631ee2-bc36-6fc4-7263-08c298121e4e>
2012-09-24 15:59:27 +0000

    添加知识搜索功能

--- MBDPrtFW/MBDPrtFstDesignMod.m/LocalInterfaces/PrtFstKnowledgeMainBoltDlg.h
+++ MBDPrtFW/MBDPrtFstDesignMod.m/LocalInterfaces/PrtFstKnowledgeMainBoltDlg.h
@@ -66,7 +66,7 @@ class PrtFstKnowledgeMainBoltDlg: public CATDlgDialog
 
 
  // Ù–‘Õ∑√˚≥∆
- CATUnicodeString m_lstStrPropertyName[9];
+ CATUnicodeString m_lstStrPropertyName[13];
 
 //END CAA2 WIZARD WIDGET DECLARATION SECTION
 };
